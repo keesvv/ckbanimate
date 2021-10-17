d4ab1a41482e4376a3f236de43a8c1e8ee0ebbf5
Kees van Voorthuizen <me@keesvv.nl>
2021-10-17 16:48:12 +0200

    feat(keymap): further complete the keymap

--- src/board.rs
+++ src/board.rs
@@ -7,7 +7,19 @@ impl Loc {
 }
 
 pub fn get_keymap() -> Vec<Vec<&'static str>> {
-    return vec![vec![
-        "esc", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12",
-    ]];
+    return vec![
+        vec![
+            "esc", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12",
+        ],
+        vec![
+            "grave", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "minus", "equal", "bspace",
+        ],
+        vec![
+            "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "lbrace", "rbrace", "bslash",
+        ],
+        vec![
+            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "colon", "quote", "enter",
+        ],
+        // TODO: finish the keymap
+    ];
 }
